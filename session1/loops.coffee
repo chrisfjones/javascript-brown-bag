10a2fbe0250cc143aa19a172b89172dec2601f1f
Chris Jones <chris.f.jones@gmail.com>
2011-10-10 15:24:43 -0700

    ecmascript section

--- session1/loops.coffee
+++
@@ -1,2 +0,0 @@
-for i in [1..10]
-  @funcs.push = (param) => i + param
--- session1/loops.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var i;
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  for (i = 1; i <= 10; i++) {
-    this.funcs.push = __bind(function(param) {
-      return i + param;
-    }, this);
-  }
-}).call(this);
